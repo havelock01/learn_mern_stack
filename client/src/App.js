6c843783f502636e1c37dc34ea929f9c1b703036
Muhammad Rizqi <74269013+havelock01@users.noreply.github.com>
2025-04-07 22:39:54 +0700

    navbar lesson done

--- client/src/App.js
+++ client/src/App.js
@@ -5,9 +5,13 @@ import About from "./pages/About";
 import ArticlesList from "./pages/ArticlesList";
 import Article from "./pages/Article";
 
+//components
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
     <Router>
+      <Navbar />
       <div className="max-w-screen-md mx-auto pt-20">
         <Routes>
           <Route path="/" element={<Home />} />
