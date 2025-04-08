a6d93ab311206b5ea260b5d2b50d407e6e68383c
Muhammad Rizqi <74269013+havelock01@users.noreply.github.com>
2025-04-08 10:01:43 +0700

    Done lesson for this branch

--- client/src/pages/Article.js
+++ client/src/pages/Article.js
@@ -1,11 +1,21 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import articleContent from "./article-content";
 
 const Article = () => {
+  const { name } = useParams();
+  const article = articleContent.find((article) => article.name === name);
+  if (!article) {
+    return <h1>Article does not exist</h1>;
+  }
   return (
     <div>
-      <h1 className="sm:text-41 text-2xl font-bold my-6 text-gray-900">
-        This is Article
+      <h1 className="my-6 text-2xl font-bold text-gray-900 sm:text-41">
+        {article.title}
       </h1>
+      {article.content.map((paragraph, index) => (
+        <p className="mx-auto mb-4 text-base leading-relaxed">{paragraph}</p>
+      ))}
     </div>
   );
 };
